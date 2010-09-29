efbd0998e95ba1dcbc08f2046a08d0f98d430418
yuanying <yuanying@fraction.jp>
2010-09-29 12:09:21 +0900

    initial project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rana4::Application.config.secret_token = '3e8a150b85a1f0a6adf64205e553082900515de2584972cc88517e319ec711eb8bdc16f3dc26bc6bcbd0004a231d1ed32d68a113cf0f71d3ca169d9a95a8c6b6'

c25f3bae177888f8797ceb70e5a1dc5e7e85f44d
yuanying <yuanying@fraction.jp>
2010-10-05 17:07:12 +0900

    画面 ID を画面に含めるように修正。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
   before_filter CASClient::Frameworks::Rails::Filter
   before_filter :load_user
   
+  def render_to_body(options)
+    @screeen_id = "#{options[:prefix]}/#{options[:template]}"
+    super
+  end
+  
   protected
   def load_user
     @user = User.find_or_create_by_cas_id(session[:cas_user])

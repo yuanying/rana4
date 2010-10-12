96daf8a530645fe51b44f7c627bef508049e71a8
yuanying <yuanying@fraction.jp>
2010-10-12 11:28:40 +0900

    jquery を使ってアニメーションを試してみた。

--- public/javascripts/attachments.js
+++ public/javascripts/attachments.js
@@ -3,7 +3,9 @@ $(function() {
   if (attachment_list_item.length > 0) {
     attachment_list_item = parent.window.$(attachment_list_item.html());
     attachment_list_item.attr('class', 'new');
+    // attachment_list_item.css({backgroundColor : 'red'});
     parent.window.$('table.attachments tbody').append(attachment_list_item);
+    // attachment_list_item.animate({backgroundColor : 'transparent'});
   } else {
     alert('Invalid Image.');
   }
